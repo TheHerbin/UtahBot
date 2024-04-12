056aad4bb99463200bd425df2fa3ea4c315b21d8
Theo Herbin <theo.herbin59710@outlook.fr>
2024-04-12 22:56:41 +0200

    working translations

--- commands/translate.js
+++ commands/translate.js
@@ -1,7 +1,5 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
 
-
-
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("translate")
@@ -14,8 +12,29 @@ module.exports = {
         ),
         execute: async ({client, interaction}) => {
             console.log("executing command : Translate ")
-            await interaction.reply('Pong!');
+
+            translatedText = await translate(interaction.options.getString("text"))
+            console.log(translatedText)
+            await interaction.reply({
+                content: translatedText
+            });
 
         }
 
+}
+
+async function translate(inputText) {
+    const inputLanguage = "en";
+    const outputLanguage = "fr";
+    const url = `https://translate.googleapis.com/translate_a/single?client=gtx&sl=${inputLanguage}&tl=${outputLanguage}&dt=t&q=${encodeURI(inputText)}`;
+
+    try {
+        const response = await fetch(url);
+        const json = await response.json();
+        const translatedText = JSON.parse(JSON.stringify(json[0]))[0][0];
+        return translatedText;
+    } catch (error) {
+        console.error(error);
+        throw new Error('Translation failed');
+    }
 }
